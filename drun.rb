6fca1cbca709cedd56ca68f087e02bf90430639c
JP Bochi <joao.bochi@rackspace.com>
2016-08-16 20:19:30 +0200

    adds drun@v0.4.1

--- drun.rb
+++ drun.rb
@@ -4,9 +4,9 @@
 class Drun < Formula
   desc "A docker run that automatically mounts your current directory"
   homepage "https://github.com/jpbochi/drun"
-  url "https://github.com/jpbochi/drun/archive/v0.4.tar.gz"
-  version "0.4"
-  sha256 "c0b40106da4e8bfc70d33adea2e3d84e5d2d897a7c75458c899a562c43e8a8d9"
+  url "https://github.com/jpbochi/drun/archive/v0.4.1.tar.gz"
+  version "0.4.1"
+  sha256 "275a3e7602ab99afe5b2b83319d88662bb8e6d2b5cf4a335ec883ddf279541f7"
   # to get new sha256, do `wget --no-verbose -O - https://... | shasum -a 256`
 
   def install
